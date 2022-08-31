43c6145a84e7963e38b104b6e2255f12078da028
Bruno Rios <Bruno.Rios@endava.com>
2022-08-31 17:34:18 -0300

    Add solutions for model excersices

--- exercises/models/crud.js
+++ exercises/models/crud.js
@@ -1,22 +1,27 @@
 const User = require('./user')
 
 const getUserById = (id) => {
- 
+  const user = User.findById(id).exec()
+  return user
 }
 
 const getAllUsers = () => {
-  
+  const users = User.find({}).exec()
+  return users
 }
 
 const createUser = (userDetails) => {
-  
+  const user = User.create(userDetails)
+  return user
 }
 const removeUserById = (id) => {
-  
+  const user = User.findByIdAndDelete(id).exec()
+  return user
 }
 
 const updateUserById = (id, update) => {
-
+  const user = User.findByIdAndUpdate(id, update, { new: true }).exec()
+  return user
 }
 
 module.exports = {
